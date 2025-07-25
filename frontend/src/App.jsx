1e59ac2348e28fe64848ac08ab4b44508bf0b534
pratik <pratikchavan11296@gmail.com>
2025-07-25 15:05:46 +0530

    Navigation and Product

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+// import Navigation from './customer/components/Navigation/Navigation'
+//import { Navigation } from './customer/components/Navigation/navigation.js';
+import Navigation from './customer/components/Navigation/Navigation';
+import Product from './customer/components/Product/Product';
+
+
+import HomePage from './customer/pages/HomePage/HomePage'
+import Footer from './customer/components/Footer/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <Navigation/>
+        <div>
+          {/* <HomePage/> */}
+          <Product/>
+        </div>
+        <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default App
